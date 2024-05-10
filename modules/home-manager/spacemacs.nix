7730bbc80cd25f8bfcbd3ab0cfe186342eb0daee
Leo D'Angelo <ldangelo@Leos-MacBook-Pro.local>
2024-05-10 13:02:45 -0500

    Got spacemacs working with nix

--- modules/home-manager/spacemacs.nix
+++ modules/home-manager/spacemacs.nix
@@ -25,62 +25,18 @@ in
 
 
 
-  xdg.configFile = {
-    "emacs" = {
-      source = builtins.path {
-        name = "emacs-dir";
-        path = ./emacs-init;
-      };
-      recursive = true;
-    };
-  };
-
-  home.packages = with pkgs; [
-    emacs-all-the-icons-fonts
-    (aspellWithDicts (d: [d.en d.sv]))
-    ghostscript
-    shellcheck
-    # tetex # Does not build properly (2024-03-09)
-    poppler
-    mu
-    wordnet
-  ];
-}
-{ config, pkgs, ... }:
-
-
-let my-emacs = pkgs.emacsMacport.override {
-      withNativeCompilation = true;
-      withSQLite3 = true;
-      withTreeSitter = true;
-      withWebP = true;
-    };
-
-    my-emacs-with-packages = (pkgs.emacsPackagesFor my-emacs).emacsWithPackages (epkgs: with epkgs; [
-      mu4e
-      vterm
-      multi-vterm
-      pdf-tools
-      treesit-grammars.with-all-grammars
-    ]
-    );
-in
-{
-  programs.emacs = {
-    enable = true;
-    package = my-emacs-with-packages;
-  };
-
-
-  home.file = ".emacs.d" = {
+  home.file.".emacs.d" = {
+    # don't make the directory read only so that impure melpa can still happen
+    # for now
     recursive = true;
     source = pkgs.fetchFromGitHub {
       owner = "syl20bnr";
       repo = "spacemacs";
-      rev = "26b8fe0c317915b622825877eb5e5bdae88fb2b2";
-      sha256 = "00cfm6caaz85rwlrbs8rm2878wgnph6342i9688w4dji3dgyz3rz";
+      rev = "develop";
+      sha256 = "sha256-D5uI9nIf0Ocxs6ZPj9/BebFM81bizZdSAHRu43csuMA=";
     };
   };
+
 
   home.packages = with pkgs; [
     emacs-all-the-icons-fonts
