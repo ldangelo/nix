099317026bf6929332bb93e0b75830d9a1920481
Leo D'Angelo <ldangelo@Leos-MacBook-Pro.local>
2024-05-11 10:28:52 -0500

    removed aliases.nix to remove confusion
    they are under homemanager

--- aliases.nix
+++
@@ -1,6 +0,0 @@
-	{ pkgs, misc, ... }: {
-	   home.shellAliases = {
-	    "darwin-build" = "pushd ~/.config/nix-darwin; darwin-rebuild --flake .#ldangelo build; popd";
-	    "hm-switch" = "pushd ~/.config/nix-darwin; darwin-rebuild --flake .#ldangelo switch; popd";
-	    };
-}
