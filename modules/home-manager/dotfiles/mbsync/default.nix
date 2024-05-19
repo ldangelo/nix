7cad241da1ce7feb35ad4f63add2b5160a19e8fd
Leo D'Angelo <ldangelo@Leos-MacBook-Pro.local>
2024-05-19 11:26:12 -0500

    Configuration for notmuch, alot, mbsync

--- modules/home-manager/dotfiles/mbsync/default.nix
+++ modules/home-manager/dotfiles/mbsync/default.nix
@@ -1,56 +1,76 @@
 { pkgs, environment, ... }: {
   # output .mbsyncrc file
-  programs.mbsync.enable = true;
-  programs.mbsync.package = pkgs.isync-oauth2;
-  programs.msmtp.enable = true;
-
-  accounts.email = {
-    accounts.icloud = {
-      address = "ldangelo@mac.com";
-      imap.host = "imap.mail.me.com";
-      userName = "ldangelo@mac.com";
-      passwordCommand = "op item get mbsync-icloud --fields password";
-      mbsync = {
-        enable = true;
-        create = "maildir";
-      };
-      msmtp.enable = true;
-      primary = true;
-      realName = "Leo A. DAngelo";
-      signature = {
-        text = ''
-             -LeoD
-
-             Leo A. DAngelo
-             ldangelo@mac.com
+  programs.mbsync = {
+    enable = true;
+    package = pkgs.isync-oauth2;
+    extraConfig = ''
+     # Keeps timestamp mased message sorting intactt
+     CopyArrivalDate = yes;
+     SyncState = *
 '';
-        showSignature = "append";
-      };
-      smtp = {
-        host = "smtp.mail.me.com";
-      };
+  };
+
+  programs.msmtp.enable = true;
+  programs.notmuch = {
+    enable = true;
+
+    new.tags = [
+      "new"
+    ];
+    hooks = {
+      preNew = "mbsync -all";
+      postNew = "afew --tag --new";
     };
-    accounts.fortium = {
-     address = "leo.dangelo@fortiumpartners.com";
-     imap.host = "imap.gmail.com";
-     imap.tls.enable = true;
-     userName = "leo.dangelo@fortiumpartners.com";
-     passwordCommand = "pizauth show fortium";
-     mbsync = {
-       enable = true;
-       create = "both";
-       expunge = "both";
-       extraConfig.account = {
-         AuthMechs = "XOAUTH2";
-       };
-     };
-     msmtp.enable = true;
-     primary = false;
-     realName = "Leo A. D'Angelo";
-     smtp = {
-       host = "localhost";
-       port = 1465;
-     };
+  };
+
+  accounts = {
+
+    email = {
+      accounts.icloud = {
+        address = "ldangelo@mac.com";
+        imap.host = "imap.mail.me.com";
+        userName = "ldangelo@mac.com";
+        passwordCommand = "op item get mbsync-icloud --fields password";
+        mbsync = {
+          enable = true;
+          create = "maildir";
+        };
+        notmuch.enable = true;
+        msmtp.enable = true;
+        primary = true;
+        realName = "Leo A. DAngelo";
+        signature = {
+          text = ''
+            -LeoD
+
+            Leo A. DAngelo
+            ldangelo@mac.com
+          '';
+          showSignature = "append";
+        };
+        smtp = { host = "smtp.mail.me.com"; };
+      };
+      accounts.fortium = {
+        address = "leo.dangelo@fortiumpartners.com";
+        imap.host = "imap.gmail.com";
+        imap.tls.enable = true;
+        userName = "leo.dangelo@fortiumpartners.com";
+        passwordCommand = "pizauth show fortium";
+        mbsync = {
+          enable = true;
+          create = "both";
+          expunge = "both";
+          extraConfig.account = { AuthMechs = "XOAUTH2"; };
+        };
+        msmtp.enable = true;
+        notmuch.enable = true;
+        primary = false;
+        realName = "Leo A. D'Angelo";
+        smtp = {
+          host = "localhost";
+          port = 1465;
+        };
+      };
     };
   };
 }
