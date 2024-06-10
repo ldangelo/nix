f8787e55123e26bf6580de8e1e22fd9fb19068bf
Leo A. D'Angelo <ldangelo@mac.com>
2024-06-10 16:07:34 -0500

    Added w3m
    Need to add support for .mailcap file.  Needs research.

---
+++ snowfall/modules/home/programs/terminal/tools/w3m/default.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+
+  cfg = config.${namespace}.programs.terminal.tools.tmux;
+in {
+  options.${namespace}.programs.terminal.tools.w3m = {
+    enable = mkBoolOpt false "Whether or not to enable w3m";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [ w3m ];
+  };
+
+}
