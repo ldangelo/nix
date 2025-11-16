3eb72ce0cfeac6a201742636a5fe5795de98a8d3
Leo A. D'Angelo <ldangelo@mac.com>
2025-11-16 11:24:10 -0600

    Fix syntax error with nvim config

--- modules/home-manager/nvim/default.nix
+++ modules/home-manager/nvim/default.nix
@@ -5,7 +5,7 @@
   home.packages = [ pkgs.neovim ];
 
   # Copy the entire nvim configuration from nix repo
-  home.file."conIg/nvim" = {
+  home.file.".config/nvim" = {
       source=../../../dotfiles/config/nvim;
       recursive=true;
   };
