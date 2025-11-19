049844e624819a6b2fbd96b9da76b3ec872e1041
Leo A. D'Angelo <ldangelo@mac.com>
2025-11-18 21:32:54 -0600

    Fix border color

--- modules/darwin/services.nix
+++ modules/darwin/services.nix
@@ -71,7 +71,7 @@
   # Jankyboarders: enable
   services.jankyborders = {
       enable = true;
-      background_color =  0xffe2e2e3;
-      inactive_color = 0xff414550;
+      active_color =  "0xffe2e2e3";
+      inactive_color = "0xff414550";
     };
 }
