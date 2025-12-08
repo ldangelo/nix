06c2cb283227ae86acc8f9a16dc195898bb6d6d6
Leo A. D'Angelo <ldangelo@mac.com>
2025-12-08 09:41:19 -0600

    Fixied shell to not load interactive parts in non-interactive startup
    wrapped plugins, etc..  in interactive check
    Added antigravity to packages
    Added zellij to packages
    Added ghosty to packages

--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -5,7 +5,11 @@
     # Development Tools
     atuin
     act                    # Run GitHub Actions locally
-    antigravity-fhs
+    antigravity
+    zellij
+    tmux
+    ghostty
+
     # aider - not in nixpkgs, available via brew
     ast-grep               # Code searching, linting, rewriting
     # basedpyright - not in nixpkgs
