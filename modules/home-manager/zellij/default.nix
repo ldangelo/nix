c7e7a1089f22ad0fd16ca6887ab10c7fc38f7500
Leo A. D'Angelo <ldangelo@mac.com>
2026-01-21 08:46:46 -0600

    The zellij directories didn't work

--- modules/home-manager/zellij/default.nix
+++ modules/home-manager/zellij/default.nix
@@ -2,7 +2,7 @@
 
 {
   xdg.configFile."zellij/config.kdl".source = ./config.kdl;
-  xdg.configFile."zellij/layouts".source = ./layouts;
+  xdg.configFile."zellij/layouts".source = ./config;
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
