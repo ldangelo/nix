c1ea87f239b58f33d47ae67356b3b7120a0e38c6
Leo D'Angelo <ldangelo@mac.com>
2026-02-23 17:10:02 -0600

    feat(nvim): enable snacks.nvim extras (zen, dim, scroll, indent, image, scope, explorer, words)

---
+++ dotfiles/config/nvim/lua/plugins/snacks/init.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dim = { enabled = true },
+    zen = { enabled = true },
+    scroll = { enabled = true },
+    indent = { enabled = true },
+    image = { enabled = true },
+    scope = { enabled = true },
+    explorer = { enabled = true },
+    words = { enabled = true },
+  },
+  keys = {
+    { "<leader>z", function() Snacks.zen() end, desc = "Zen Mode" },
+    { "<leader>Z", function() Snacks.zen.zoom() end, desc = "Zen Zoom" },
+    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+    { "]]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+    { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+  },
+}
