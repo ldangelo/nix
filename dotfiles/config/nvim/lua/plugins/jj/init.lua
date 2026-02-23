b1a64a2aed244f450256aa275788c55d76fb32a9
Leo D'Angelo <ldangelo@mac.com>
2026-02-23 17:10:02 -0600

    feat(nvim): add jj.nvim leader keybindings

--- dotfiles/config/nvim/lua/plugins/jj/init.lua
+++ dotfiles/config/nvim/lua/plugins/jj/init.lua
@@ -22,6 +22,24 @@ return {
         renamed = { fg = "#d29922", ctermfg = "Yellow" }, -- Renamed files
       },
     })
-    -- Use the exposed functions directly from the main module
   end,
+  keys = {
+    { "<leader>jl", function() require("jj.log").log() end, desc = "Log" },
+    { "<leader>js", function() require("jj.status").status() end, desc = "Status" },
+    { "<leader>jd", function() require("jj.diff").diff() end, desc = "Diff" },
+    { "<leader>jD", function() require("jj.diff").diff_current() end, desc = "Diff current file" },
+    { "<leader>jn", function() require("jj.commands").new() end, desc = "New change" },
+    { "<leader>je", function() require("jj.commands").edit() end, desc = "Edit change" },
+    { "<leader>jc", function() require("jj.describe").commit() end, desc = "Describe/commit" },
+    { "<leader>jq", function() require("jj.commands").squash() end, desc = "Squash" },
+    { "<leader>jr", function() require("jj.commands").rebase() end, desc = "Rebase" },
+    { "<leader>jp", function() require("jj.push").push() end, desc = "Push" },
+    { "<leader>jf", function() require("jj.commands").fetch() end, desc = "Fetch" },
+    { "<leader>jb", function() require("jj.bookmark").bookmark_create() end, desc = "Create bookmark" },
+    { "<leader>jB", function() require("jj.browse").browse() end, desc = "Browse (open PR)" },
+    { "<leader>jh", function() require("jj.file").file_history() end, desc = "File history" },
+    { "<leader>ja", function() require("jj.commands").abandon() end, desc = "Abandon change" },
+    { "<leader>ju", function() require("jj.commands").undo() end, desc = "Undo" },
+    { "<leader>jR", function() require("jj.commands").redo() end, desc = "Redo" },
+  },
 }
